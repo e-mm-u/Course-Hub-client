06e96ab901dfee40420f3b6210140d711b897447
e-mm-u <uh.habiba1012@gmail.com>
2022-10-25 00:00:43 +0600

    login page structure is built

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 import routes from './routes/Routes';
 
 function App() {
--- src/components/logs/Login.js
+++ src/components/logs/Login.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Login = () => {
     return (
-        <div>
-            login
+        <div className='w-25 mx-auto'>
+            <Form >
+                <Form.Group className="mb-3" controlId="form-email">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control name='email' type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="form-password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control name='password' type="password" placeholder="Password" />
+                </Form.Group>
+                <div className='d-flex justify-content-between'>
+                    <Form.Group className="mb-3" controlId="form-checkbox">
+                        <Form.Check type="checkbox" label="Remember me" />
+                    </Form.Group>
+                    <p>Forgot password?</p>
+                </div>
+
+
+                <Form.Group className="mb-3">
+                    <Form.Text className="text-muted">
+                        error
+                    </Form.Text>
+                </Form.Group>
+
+                <div className='d-flex justify-content-between align-items-center'>
+                    <Button variant="light" type="submit">
+                        Log in
+                    </Button>
+                    <LinkContainer to='/register'>
+                        <p className='mb-0'> Don't have an account? </p>
+                    </LinkContainer>
+                </div>
+
+
+            </Form>
+            <div className='d-flex align-items-center justify-content-center gap-2'>
+                <hr className='w-25' /> <p className='mb-0'>OR</p> <hr className='w-25' />
+            </div>
+            {/* sign up buttons */}
+            <div className='d-flex flex-column gap-2'>
+                <Button variant="light">
+                    Continue with google
+                </Button>
+                <Button variant="light">
+                    Continue with github
+                </Button>
+                <Button variant="light">
+                    Continue with facebook
+                </Button>
+            </div>
         </div>
     );
 };
