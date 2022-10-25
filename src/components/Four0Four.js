2473de8e003f77a479f3a91ae45499c5256df6e1
e-mm-u <uh.habiba1012@gmail.com>
2022-10-26 01:22:31 +0600

    404 modified

--- src/components/Four0Four.js
+++ src/components/Four0Four.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 
 const Four0Four = () => {
     return (
-        <div>
-            404
+        <div className='d-flex my-5 justify-content-center align-items-center'>
+            <Image
+                src='https://img.freepik.com/premium-vector/error-404-vector-illustration-page-found-road-sign-clouds-incorrect-request-search-website-online-loading-internet-concept-vector-line-icon-business-advertising_661108-1787.jpg'
+
+                alt ='404 image'
+            />
         </div>
     );
 };
