6d8d67744ac800eb262b41fd4a28e63a91be5819
e-mm-u <uh.habiba1012@gmail.com>
2022-10-26 22:00:16 +0600

    course-detail re-designed

--- src/components/courses/CourseDetails.js
+++ src/components/courses/CourseDetails.js
@@ -4,21 +4,26 @@ import { useLoaderData } from 'react-router-dom';
 
 const CourseDetails = () => {
     const course = useLoaderData();
-    const {photoURL, title, details, rating, fee, requiredTime} = course;
+    const { photoURL, title, details, rating, fee, requiredTime } = course;
     console.log(photoURL, title)
     return (
-        <div>
-            <div>
-            <Image src={photoURL} style={{height : '300px', width: '4000px'}}/>
-            <h6>{title}</h6>
-            <p>{details}...</p>
-            <span>{rating}  </span>
-            <span>{requiredTime}  </span>
-            <span>{fee}</span>
-
-            <Button>Enroll</Button>
-            <Button>Get premium</Button>
-        </div>
+        <div className='my-3 d-flex justify-content-center'>
+            <div className='card w-75 border-0'>
+                <Image className='w-85 rounded' src={photoURL} style={{ height: '30vw' }} />
+                <div className='card-body'>
+                    <h5 className='card-title'>{title}</h5>
+                    <p className='card-text'>{details}...</p>
+                </div>
+                <div className='card-body d-flex gap-3'>
+                    <span>{rating}  </span>
+                    <span>{requiredTime}  </span>
+                    <span>{fee}$</span>
+                </div>
+                <div className="card-body d-flex gap-2">
+                    <Button>Enroll</Button>
+                    <Button>Get premium</Button>
+                </div>
+            </div>
         </div>
     );
 };
