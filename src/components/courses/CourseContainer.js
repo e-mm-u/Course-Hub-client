b147ab392feeeb7439deac4478153e85fdc00401
e-mm-u <uh.habiba1012@gmail.com>
2022-10-26 22:36:41 +0600

    course details and card designed

--- src/components/courses/CourseContainer.js
+++ src/components/courses/CourseContainer.js
@@ -5,10 +5,11 @@ import CourseSummaryCard from './CourseSummaryCard';
 const CourseContainer = () => {
     const courses = useLoaderData();
     return (
-        <div className='d-flex flex-wrap gap-4 justify-content-center'>
+        // d-flex flex-wrap gap-3 justify-content-center
+        <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 my-3'> 
             {
                 courses.map(course =>
-                    <div key={course.course_id}>
+                    <div className='col' key={course.course_id}>
                         <CourseSummaryCard course={course}> </CourseSummaryCard>
                     </div>
                 )
--- src/components/courses/CourseSummaryCard.js
+++ src/components/courses/CourseSummaryCard.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { Button, Image } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const CourseSummaryCard = ({course}) => {
-    const {course_id, photoURL, title, details, rating, fee, requiredTime} = course;
+const CourseSummaryCard = ({ course }) => {
+    const { course_id, photoURL, title, details, rating, fee, requiredTime } = course;
     return (
-        <div>
-            <Image src={photoURL} style={{height : '100px', width: '250px'}}/>
-            <h6>{title}</h6>
-            <p>{details.slice(0,100)}...</p>
-            <span>{rating}  </span>
-            <span>{requiredTime}  </span>
-            <span>{fee}</span>
-            <LinkContainer to={`/courses/course/${course_id}`}><Button>Details</Button></LinkContainer>
-            
+        <div className='card border-1 h-100'>
+            <Image className='card-img rounded ' src={photoURL} style={{ height: '50%', }} />
+            <div className="card-body">
+                <h6>{title}</h6>
+                <p>{details.slice(0, 100)}...</p>
+            </div>
+            <div className="card-body">
+                <div className='d-flex gap-4'>
+                    <span>{rating}  </span>
+                    <span>{requiredTime}  </span>
+                    <span>{fee}</span>
+                </div>
+                <LinkContainer to={`/courses/course/${course_id}`}><Button className='w-100 mt-3'>Details</Button></LinkContainer>
+            </div>
+            {/* style={{height : '100px', width: '250px'}} */}
         </div>
     );
 };
