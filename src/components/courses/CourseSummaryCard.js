a76f5780bf28179fe1847cd53ea645e4169f0b31
e-mm-u <uh.habiba1012@gmail.com>
2022-10-26 22:43:12 +0600

    design-fixed

--- src/components/courses/CourseSummaryCard.js
+++ src/components/courses/CourseSummaryCard.js
@@ -6,7 +6,7 @@ const CourseSummaryCard = ({ course }) => {
     const { course_id, photoURL, title, details, rating, fee, requiredTime } = course;
     return (
         <div className='card border-1 h-100'>
-            <Image className='card-img rounded ' src={photoURL} style={{ height: '50%', }} />
+            <Image className='card-img rounded ' src={photoURL} style={{ height: '200px', }} />
             <div className="card-body">
                 <h6>{title}</h6>
                 <p>{details.slice(0, 100)}...</p>
@@ -17,7 +17,11 @@ const CourseSummaryCard = ({ course }) => {
                     <span>{requiredTime}  </span>
                     <span>{fee}</span>
                 </div>
-                <LinkContainer to={`/courses/course/${course_id}`}><Button className='w-100 mt-3'>Details</Button></LinkContainer>
+                <div className='mt-2'>
+                    <LinkContainer to={`/courses/course/${course_id}`}>
+                        <Button className='w-100'>Details</Button>
+                    </LinkContainer>
+                </div>
             </div>
             {/* style={{height : '100px', width: '250px'}} */}
         </div>
