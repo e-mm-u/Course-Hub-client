1c778c2fa049e01dff63cdd4476d383e7019a290
e-mm-u <uh.habiba1012@gmail.com>
2022-10-27 10:19:50 +0600

    key hidden done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,14 +5,29 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDIqqzBMETRdrAPA5M3dnBJk5O0Np0N-ps",
-    authDomain: "course-hub-69d38.firebaseapp.com",
-    projectId: "course-hub-69d38",
-    storageBucket: "course-hub-69d38.appspot.com",
-    messagingSenderId: "719167481808",
-    appId: "1:719167481808:web:19e59b3a149eb60b9c7bd5"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
+
+// apiKey: "AIzaSyDIqqzBMETRdrAPA5M3dnBJk5O0Np0N-ps",
+// authDomain: "course-hub-69d38.firebaseapp.com",
+// projectId: "course-hub-69d38",
+// storageBucket: "course-hub-69d38.appspot.com",
+// messagingSenderId: "719167481808",
+// appId: "1:719167481808:web:19e59b3a149eb60b9c7bd5"
+
+// apiKey: process.env.REACT_APP_authDomain ,
+// authDomain: process.env.REACT_APP_apiKey ,
+// projectId: process.env.REACT_APP_projectId ,
+// storageBucket: process.env.REACT_APP_storageBucket ,
+// messagingSenderId: process.env.REACT_APP_messagingSenderId ,
+// appId: process.env.REACT_APP_appId 
